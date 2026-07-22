b5724e7e0ed5788927ab71c61df135de91b7f014
killbot-oss <asiatrade226@gmail.com>
2026-07-22 16:54:49 +0000

    Update config.js

--- laafi-connect/frontend/www/js/config.js
+++ laafi-connect/frontend/www/js/config.js
@@ -8,7 +8,6 @@
 window.LAAFI_CONFIG = {
   API_BASE_URL: 'https://chronos-nc82.onrender.com/api/v1',
   API_HEALTH_URL: 'https://chronos-nc82.onrender.com/health',
-  // Set to true while developing locally with `uvicorn` on your machine.
   USE_LOCAL_BACKEND: false,
 };
 
